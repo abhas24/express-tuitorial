1b998440cec3e0ff3f1418f4952d5949bd525fa0
Anurag Bhaskar <anuragbhaskar24@gmail.com>
2019-08-20 08:36:27 +0530

    Post method example with input validation using Joi package

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const Joi = require('joi');// Will return a class
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 const courses = [
 	{id:1, name:'course1'},
 	{id:2, name:'course2'},
@@ -23,6 +25,31 @@ app.get('/api/course/:id',(req,res) => {
 	});
 
 	if(!course) res.status(404).send('The course with the given id was not found.');
+	res.send(course);
+});
+
+//post methods example with param (name) validation
+//Here we are using joi npm package for validating the input
+app.post('/api/courses',(req,res)=>{
+
+	const schema = {
+		name:Joi.string().min(3).required()
+	};
+
+	const result = Joi.validate(req.body,schema);
+	//const { error } = Joi.validate(req.body,schema); 
+	//const { error } is same as result.error
+	// { error } is known as object destructuring. 
+	console.log(result);
+	if(result.error){
+		return res.status(400).send(result.error.details[0].message);
+	}
+	var course = {
+		id:courses.length+1,
+		name:req.body.name
+	}
+
+	courses.push(course);
 	res.send(course);
 });
 
