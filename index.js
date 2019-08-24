d9095e998de7b4f0df8deb0ee26f1dd974d2ec0d
Anurag Bhaskar <anuragbhaskar24@gmail.com>
2019-08-24 20:17:49 +0530

    Callback example to manage asysnc code

--- index.js
+++ index.js
@@ -1,57 +1,36 @@
-const Joi = require('joi');// Will return a class
-const express = require('express');
-const app = express();
+console.log('before');
 
-app.use(express.json());
-const courses = [
-	{id:1, name:'course1'},
-	{id:2, name:'course2'},
-	{id:3, name:'course3'}
-];
-
-app.get('/', (req,res) => {
-	res.send('Hello World');
-});
-
-// Get all couses
-app.get('/api/courses',(req,res) => {
-	res.send(courses);
-});
-
-// get single course
-app.get('/api/course/:id',(req,res) => {
-	const course = courses.find(c => {
-		if(c.id === parseInt(req.params.id)) return c;
+/*const user = getUser(1,(userDetails)=>{
+	console.log('userDetails',userDetails);
+	getrepositories(userDetails.githubUsername,(repositories)=>{
+		console.log('repositories',repositories);
+		// This coding staructure is called as callback hells
 	});
+});*/
 
-	if(!course) res.status(404).send('The course with the given id was not found.');
-	res.send(course);
-});
+const user = getUser(1,testgetrepositories);
+console.log(user);
+console.log('after');
+//Solving the problem of callback hells
+function testgetrepositories(userDetails){
+	console.log('userDetails',userDetails);
+	getrepositories(userDetails.githubUsername,displayRepositories);
+}
+function displayRepositories(repositories){
+	console.log(repositories[0]);
+}
+//Example of callback function 
+function getUser(id,callback){
+	setTimeout(()=>{
+		console.log('Reading user from a databse');
+		callback({id:id,githubUsername:'Anurag'});
+	},2000);
+}
 
-//post methods example with param (name) validation
-//Here we are using joi npm package for validating the input
-app.post('/api/courses',(req,res)=>{
 
-	const schema = {
-		name:Joi.string().min(3).required()
-	};
-
-	const result = Joi.validate(req.body,schema);
-	//const { error } = Joi.validate(req.body,schema); 
-	//const { error } is same as result.error
-	// { error } is known as object destructuring. 
-	console.log(result);
-	if(result.error){
-		return res.status(400).send(result.error.details[0].message);
-	}
-	var course = {
-		id:courses.length+1,
-		name:req.body.name
-	}
-
-	courses.push(course);
-	res.send(course);
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listing on port ${port}...`));
+function getrepositories(username,callback){
+	setTimeout(()=>{
+		console.log('Fetching the repositories...');
+		callback(['repo1','repo2','repo3']);
+	},2000);
+}
