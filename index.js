298bfab9a49b6074f48183613586993fa42f348a
Anurag Bhaskar <anuragbhaskar24@gmail.com>
2019-09-10 07:31:44 +0530

    promise example

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
-console.log('before');
+var p = new Promise(function(resolve, reject){
+	resolve(1);
+}).then(function(result){
+	console.log('result',result);
+});
 
-/*const user = getUser(1,(userDetails)=>{
-	console.log('userDetails',userDetails);
-	getrepositories(userDetails.githubUsername,(repositories)=>{
-		console.log('repositories',repositories);
-		// This coding staructure is called as callback hells
-	});
-});*/
 
-const user = getUser(1,testgetrepositories);
-console.log(user);
-console.log('after');
-//Solving the problem of callback hells
-function testgetrepositories(userDetails){
-	console.log('userDetails',userDetails);
-	getrepositories(userDetails.githubUsername,displayRepositories);
-}
-function displayRepositories(repositories){
-	console.log(repositories[0]);
-}
-//Example of callback function 
-function getUser(id,callback){
+
+var pes6 = new Promise((resolve,reject) => {
 	setTimeout(()=>{
-		console.log('Reading user from a databse');
-		callback({id:id,githubUsername:'Anurag'});
+		resolve(1);
 	},2000);
-}
-
-
-function getrepositories(username,callback){
-	setTimeout(()=>{
-		console.log('Fetching the repositories...');
-		callback(['repo1','repo2','repo3']);
-	},2000);
-}
+}).then((result)=>{
+	console.log('In ES6');
+	console.log('result',result);
+	return result;
+}).then((result)=>{
+	console.log('result',result*2);
+})
